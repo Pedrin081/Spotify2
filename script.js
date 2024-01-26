ef62c547bd291a364bf4136e080040a2fec487cc
Pedro Victor <pedro.vnascimento@outlook.com>
2024-01-26 17:58:53 -0300

    26/1

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 const greetingElement = document.getElementById("greeting");
-
-
 const currentHour = new Date().getHours();
-
- if (currentHour >= 5 && currentHour < 12) {
-   greetingElement.textContent = "Bom dia";
-} else if (currentHour >= 12 && currentHour < 18) {
-  greetingElement.textContent = "Boa tarde";
- } else {
-   greetingElement.textContent = "Boa noite";
-}
-
 
 const greetingMessage =
   currentHour >= 5 && currentHour < 12
@@ -25,7 +14,7 @@ greetingElement.textContent = greetingMessage;
 const container = document.querySelector(".offer__list-item");
 
 const observer = new ResizeObserver(() => {  
-  const containerWidth = container.offsetWidth; 
+  const containerWidth = container.offsetWidth;
   const numColumns = Math.floor(containerWidth / 200); 
 
   
@@ -34,41 +23,4 @@ const observer = new ResizeObserver(() => {
   console.log({ container });
   console.log({ numColumns });
 });
-
 observer.observe(container);
-
-const resultArtist = document.getElementById("result-artist");
-const playlistContainer = document.getElementById("result-playlists");
-const searchInput = document.getElementById("search-input");
-
-function requestApi(searchTerm) {
-  fetch(`http://localhost:3233/artists?name_like=${searchTerm}`)
-    .then((response) => response.json())
-    .then((results) => displayResults(results));
-}
-
-function displayResults(results) {
-  hidePlaylists();
-  const artistImage = document.getElementById("artist-img");
-  const artistName = document.getElementById("artist-name");
-
-  results.forEach((element) => {
-    artistImage.src = element.urlImg;
-    artistName.innerText = element.name;
-  });
-  resultArtist.classList.remove("hidden");
-}
-
-function hidePlaylists() {
-  playlistContainer.classList.add("hidden");
-}
-
-document.addEventListener("input", function () {
-    const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === "") {
-      resultplaylist.classList.remove("hidden");
-      resultArtist.classList.add("hidden");
-      return;
-    }
-    requestApi(searchTerm);
-});
